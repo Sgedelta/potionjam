6f09db9518c053526c37a6c8f25998f08961c854
numbersguy104 <numbersboy@icloud.com>
2026-05-20 12:07:00 -0400

    Touchup character movement
    Character movement uses new custom gameplay actions. Controls can be toggled on or off with SetControlsEnabled(bool).

--- Scenes/Character.cs
+++ Scenes/Character.cs
@@ -7,6 +7,16 @@ public partial class Character : CharacterBody3D
 
 	public const float Speed = 5.0f;
 	public const float JumpVelocity = 4.5f;
+	
+	private bool _controlsEnabled = true;
+	
+	public bool GetControlsEnabled() {
+		return _controlsEnabled;
+	}
+	
+	public void SetControlsEnabled(bool enabled) {
+		_controlsEnabled = enabled;
+	}
 
 	public override void _PhysicsProcess(double delta)
 	{
@@ -18,25 +28,26 @@ public partial class Character : CharacterBody3D
 			velocity += GetGravity() * (float)delta;
 		}
 
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-		{
-			velocity.Y = JumpVelocity;
-		}
+		if (_controlsEnabled) {
+			// Handle Jump.
+			if (Input.IsActionJustPressed("movement_jump") && IsOnFloor())
+			{
+				velocity.Y = JumpVelocity;
+			}
 
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
-		if (direction != Vector3.Zero)
-		{
-			velocity.X = direction.X * Speed;
-			velocity.Z = direction.Z * Speed;
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
+			// Get the input direction and handle the movement/deceleration.
+			Vector2 inputDir = Input.GetVector("movement_left", "movement_right", "movement_up", "movement_down");
+			Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+			if (direction != Vector3.Zero)
+			{
+				velocity.X = direction.X * Speed;
+				velocity.Z = direction.Z * Speed;
+			}
+			else
+			{
+				velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+				velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
+			}
 		}
 
 		Velocity = velocity;
